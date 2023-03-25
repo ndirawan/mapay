e85b74bc0114080b927fb6c223da4ea919eb08e7
Andi Irawan <andixtoel@gmail.com>
2023-03-25 10:48:42 +0700

    init

---
+++ src/main/java/com/enigma/mapay/MaPayWalletApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.mapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaPayWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaPayWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/mapay/MaPayWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.mapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaPayWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
