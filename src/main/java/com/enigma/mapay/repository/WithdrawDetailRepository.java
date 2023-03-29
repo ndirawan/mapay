4b83b1db4050d6486b6fd78c304b64c4361d291d
Andi Irawan <andixtoel@gmail.com>
2023-03-29 19:16:51 +0700

    [IMPROVE] refactor

--- src/main/java/com/enigma/mapay/repository/WithdrawDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.enigma.mapay.repository;
-
-import com.enigma.mapay.entity.WithdrawDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WithdrawDetailRepository extends JpaRepository<WithdrawDetail, String> {
-}
--- src/main/java/com/enigma/mapay/repository/WithdrawRepository.java
+++
@@ -1,8 +0,0 @@
-package com.enigma.mapay.repository;
-
-import com.enigma.mapay.entity.Withdraw;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface WithdrawRepository extends JpaRepository<Withdraw, String>, JpaSpecificationExecutor<Withdraw> {
-}
